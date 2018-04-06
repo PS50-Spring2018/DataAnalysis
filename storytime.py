371727d87c69a2d03cbb3592808c3ed7d87f22dd
Liam Power <liampower@college.harvard.edu>
2018-04-06 16:22:39 -0400

    trying to make changes L

--- storytime.py
+++ storytime.py
@@ -4,3 +4,7 @@ After every few sentences, push your changes to the remote repository and resolv
 
 YOUR STORY HERE
 '''
+It was a dark, chilly spring afternoon. The sun sank beneath the dark clouds sitting about the stormy Charles. 
+The winds howled...
+
+Liam was suprised. He wasn't sure if he was doing the whole 'pushing' and 'pulling' correctly.
