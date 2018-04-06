8a5f9a740e2fc1af714e87d82668ed56af9c53d5
SiavashZ <szamirpour@college.harvard.edu>
2018-04-06 16:14:21 -0400

    added intro

--- storytime.py
+++ storytime.py
@@ -4,3 +4,9 @@ After every few sentences, push your changes to the remote repository and resolv
 
 YOUR STORY HERE
 '''
+There once was a cat named Hamilton who lived with a family of sheep.
+No one really remembers how Hamilton came to live with the sheep.
+One day the sheep woke up to meowing and saw a cat on the edge of the field.
+"Greetings!" said the cat.
+"I am Hamilton."
+The sheep were quite content with having another addition to the group.
