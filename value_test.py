d1517309544c2a3a90c686cb34835a1b318668f4
Liam Power <liampower@college.harvard.edu>
2018-04-19 17:45:53 -0400

    value bar

--- value_test.py
+++ value_test.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 import colorsys as cs
 
 
-N = 100
-radii = np.linspace(0,1,N)
-thetas = np.linspace(0,2*np.pi,N)
-t = [] #list of thetas
-r = [] #list of radii
-c = [] #list of full of three component list, 3*n dimensional list
+N = 200
+x_dim = np.linspace(0,1,N)
+y_dim = np.linspace(0,1,N)
+x = []
+y = []
+color = []
 
 # create lists with all possible hs values with v set to 1 (first normalize, then convert to RGB)
-for theta in thetas: #nested for loop
-	for radius in radii:
-		t.append(theta)
-		r.append(radius)
-		c.append(cs.hsv_to_rgb(theta/(2*np.pi),radius,1)) #normalize all theta
+for x_val in x_dim: #nested for loop
+	for y_val in y_dim:
+		x.append(x_val)
+		y.append(y_val)
+		color.append(cs.hsv_to_rgb(0, 0, 1-x_val)) #normalize all theta
 		# 1 at the end corresponds to brightness
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(t, r, c=c, alpha=1.0) #has to take RGB values
+ax = fig.add_subplot(111)
+c = ax.scatter(x, y, c=color, alpha=1.0) #has to take RGB values
 plt.axis('off')
 
 # trying to plot example colors onto colorwheel now; hasn't worked so far
