5a5af7845054efe1f7ff65be76db981e6529cea1
Liam Power <liampower@college.harvard.edu>
2018-04-19 17:08:45 -0400

    Liam attempt

--- my_colorwheel_test.py
+++ my_colorwheel_test.py
@@ -2,34 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import colorsys as cs
 
-N = 1000
-radii = np.linspace(0,1,N)
-thetas = np.linspace(0,2*np.pi,N)
-t = [] #list of thetas
-r = [] #list of radii
-c = [] #list of full of three component list, 3*n dimensional list
+def Imagedash():
 
-# create lists with all possible hs values with v set to 1 (first normalize, then convert to RGB)
-for theta in thetas: #nested for loop
-	for radius in radii:
-		t.append(theta)
-		r.append(radius)
-		c.append(cs.hsv_to_rgb(theta/(2*np.pi),radius,1)) #normalize all theta
-		# 1 at the end corresponds to brightness
+	N = 1000
+	radii = np.linspace(0,1,N)
+	thetas = np.linspace(0,2*np.pi,N)
+	t = [] #list of thetas
+	r = [] #list of radii
+	c = [] #list of full of three component list, 3*n dimensional list
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(t, r, c=c, alpha=1.0) #has to take RGB values
-plt.axis('off')
+	# create lists with all possible hs values with v set to 1 (first normalize, then convert to RGB)
+	for theta in thetas: #nested for loop
+		for radius in radii:
+			t.append(theta)
+			r.append(radius)
+			c.append(cs.hsv_to_rgb(theta/(2*np.pi),radius,1)) #normalize all theta
+			# 1 at the end corresponds to brightness
 
-# trying to plot example colors onto colorwheel now; hasn't worked so far
-# rgb_test = np.array([255,0,0],[0,128,0],[0,0,255]) #red, green, and blue
-# t_plot = []
-# r_plot = []
-# for color in rgb_test:
-# 	t_plot.append(color[0]/255*2*np.pi)
-# 	r_plot.append(color[1]/255)
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='polar')
+	c = ax.scatter(t, r, c=c, alpha=1.0) #has to take RGB values
+	plt.axis('off')
 
-# ax.scatter(t_plot,r_plot,linestyle='xk-')
+	# trying to plot example colors onto colorwheel now; hasn't worked so far
+	# rgb_test = np.array([255,0,0],[0,128,0],[0,0,255]) #red, green, and blue
+	# t_plot = []
+	# r_plot = []
+	# for color in rgb_test:
+	# 	t_plot.append(color[0]/255*2*np.pi)
+	# 	r_plot.append(color[1]/255)
 
-plt.show()
+	# ax.scatter(t_plot,r_plot,linestyle='xk-')
+
+	plt.show()
+
+x = Imagedash()
+print(x)
--- plottest.py
+++ plottest.py
@@ -3,26 +3,59 @@ from matplotlib import image as img
 from matplotlib import gridspec as grd
 import seaborn as sns
 import numpy as np
+import colorsys as cs
+
+
+
+N = 1000
+radii = np.linspace(0,1,N)
+thetas = np.linspace(0,2*np.pi,N)
+t = [] #list of thetas
+r = [] #list of radii
+c = [] #list of full of three component list, 3*n dimensional list
+
+# create lists with all possible hs values with v set to 1 (first normalize, then convert to RGB)
+for theta in thetas: #nested for loop
+	for radius in radii:
+		t.append(theta)
+		r.append(radius)
+		c.append(cs.hsv_to_rgb(theta/(2*np.pi),radius,1)) #normalize all theta
+		# 1 at the end corresponds to brightness
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='polar')
+c = ax.scatter(t, r, c=c, alpha=1.0) #has to take RGB values
+plt.axis('off')
+
+# trying to plot example colors onto colorwheel now; hasn't worked so far
+# rgb_test = np.array([255,0,0],[0,128,0],[0,0,255]) #red, green, and blue
+# t_plot = []
+# r_plot = []
+# for color in rgb_test:
+# 	t_plot.append(color[0]/255*2*np.pi)
+# 	r_plot.append(color[1]/255)
+
+# ax.scatter(t_plot,r_plot,linestyle='xk-')
+
+
 
 x = [1,2,4]
 y = [2,5,6]
 x2 = [0.1,0.24,0.53]
 ourim = img.imread('Cat03.jpg')
-secim = img.imread('colorwheel.jpg')
 gs =grd.GridSpec(2,3)
 ax1 = plt.subplot2grid((2,3),(0,0))
 ax2 = plt.subplot2grid((2,3),(0,1))
 ax3 = plt.subplot2grid((2,3),(0,2))
 ax4 = plt.subplot2grid((2,3),(1,0), colspan=2)
-ax5 = plt.subplot2grid((2,3),(1,2))
+ax5 = plt.subplot2grid((2,3),(1,2), projection = 'polar')
 ax1.errorbar(x,y,color='r',yerr=x2)
 ax4.errorbar(x2,x,yerr=y)
 ax2.imshow(ourim)
-ax5.imshow(secim)
-
+ax5.scatter(s, r, c=c, alpha=1.0)
 ax2.xaxis.set_visible(False)
 ax2.yaxis.set_visible(False)
-for t in range(1,100):
+for s in range(1,100):
 	x = np.random.rand(3)
 	ax3.clear()
 	ax3.plot(y,x)
