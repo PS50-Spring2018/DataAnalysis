589c20e7fad961b6c39ef457b99f78222fdf2103
SiavashZ <szamirpour@college.harvard.edu>
2018-04-19 17:11:43 -0400

    updates

--- my_colorwheel_test.py
+++ my_colorwheel_test.py
@@ -22,13 +22,15 @@ c = ax.scatter(t, r, c=c, alpha=1.0)
 plt.axis('off')
 
 # trying to plot example colors onto colorwheel now; hasn't worked so far
-# rgb_test = np.array([255,0,0],[0,128,0],[0,0,255]) #red, green, and blue
-# t_plot = []
-# r_plot = []
-# for color in rgb_test:
-# 	t_plot.append(color[0]/255*2*np.pi)
-# 	r_plot.append(color[1]/255)
+rgb_test = np.array([[255,0,0],[0,128,0],[0,0,255]]) #red, green, and blue
+t_val = []
+r_val = []
+for color in rgb_test:
+	r, g, b = color[0], color[1], color[2]
+	hsv = cs.rgb_to_hsv(r,g,b)
+	t_val.append(hsv[0]*2*np.pi)
+	r_val.append(hsv[1])
 
-# ax.scatter(t_plot,r_plot,linestyle='xk-')
+d = ax.plot(t_val,r_val, 'ko-')
 
 plt.show()
